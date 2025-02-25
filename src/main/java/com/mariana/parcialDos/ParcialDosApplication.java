25414ae5926b952cefcce0c79a71c6ac3e4735e7
mgonzalez96 <marianaacevedo1996@gmail.com>
2025-02-24 20:32:48 -0500

    Primer commit

---
+++ src/main/java/com/mariana/parcialDos/ParcialDosApplication.java
@@ -0,0 +1,13 @@
+package com.mariana.parcialDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariana/parcialDos/ParcialDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariana.parcialDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
